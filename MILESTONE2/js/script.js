cfe338df46277a5c4f2430df292ee8ea3c994391
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-17 16:04:18 +0200

    milestone2-full

--- MILESTONE2/js/script.js
+++ MILESTONE2/js/script.js
@@ -9,14 +9,25 @@ const images = [
     '05.jpg',
 ];
 
-
 // inserisco le immagini nell html
-const slider = document.querySelector('.container');
+const slider = document.querySelector('.slides');
 
 let imgTags = '';
 
  for(i = 0; i < images.length; i++){
-    imgTags += `<img src="img/${images[i]}" alt="${images[i]}" class="item">`;
+    imgTags += `<img src="img/${images[i]}" alt="${images[i]}" class="item hide">`;
  }
 
 slider.innerHTML = imgTags;
+
+//console.log(slider);
+
+//salvo in una collection tutti gli elementi html che hanno la classe 'item'
+
+const items = document.getElementsByClassName('item');
+
+//rimuovo d-none al primo elemento dell array
+
+let counterImages = 0;
+items[counterImages].classList.remove('hide');
+
